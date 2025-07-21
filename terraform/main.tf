4290c6be0d7f1b053ca299a1fcf92b559aba42ec
lakshmiLearning <lakshmisrlearning@gmail.com>
2025-07-21 21:04:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "githubaction-demo-bucket-newone"  
+  bucket = "githubaction-demo-bucket-newone-12343223"  
 
   tags = {
     CreatedBy = "Terraform"
